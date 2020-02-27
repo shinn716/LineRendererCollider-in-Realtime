6e650d8cd650400b020bf435f268605c3f959871
Tung <shinn716@livemail.tw>
2020-02-27 13:48:46 +0800

    Add files via upload
    Upload script

---
+++ AttachLineRenderer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AttachLineRenderer : MonoBehaviour
+{
+    LineRenderer line;
+    CapsuleCollider capsule;
+
+    //public float LineWidth; // use the same as you set in the line renderer.
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        capsule = gameObject.AddComponent<CapsuleCollider>();
+        capsule.radius = line.startWidth / 2;
+        capsule.center = Vector3.zero;
+        capsule.direction = 2; // Z-axis for easier "LookAt" orientation
+    }
+
+    void Update()
+    {
+        Vector3 start = line.GetPosition(0);
+        Vector3 target = line.GetPosition(1);
+        
+        capsule.transform.position = start + (target - start) / 2;
+        capsule.transform.LookAt(start);
+        capsule.height = (target - start).magnitude;
+    }
+}
---
+++ LineRenderCollderManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LineRenderCollderManager : MonoBehaviour
+{   
+    public struct LRCol
+    {
+        public Vector3 start;
+        public Vector3 end;
+    }
+
+    LRCol[] LRlist;
+    LineRenderer line;
+
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+        LRlist = new LRCol[line.numPositions - 1];
+
+        StartCoroutine(Init());
+    }
+
+    private IEnumerator Init()
+    {
+        yield return new WaitForEndOfFrame();
+        for (int i = 0; i < line.numPositions - 1; i++)
+        {
+            LRlist[i].start = line.GetPosition(i);
+            LRlist[i].end = line.GetPosition(i + 1);
+
+            GameObject LRChild = new GameObject();
+            LRChild.name = "LRChild" + (i + 1);
+            LRChild.transform.SetParent(transform);
+
+            LRChild.AddComponent<LineRenderer>();
+
+            LineRenderer linec = LRChild.GetComponent<LineRenderer>();
+            linec.numPositions = 2;
+            linec.SetPosition(0, LRlist[i].start);
+            linec.SetPosition(1, LRlist[i].end);
+            linec.enabled = false;
+            linec.gameObject.AddComponent<AttachLineRenderer>();
+        }
+    }
+}
